8e0eb017fc855c0162658c11f415649b28098be1
cimadaren <darenarnold1@Darens-MBP.fios-router.home>
2017-08-01 18:28:25 -0400

    Working on JSONPath issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 var https = require('https');
 var Alexa = require('alexa-sdk');
 
-var JSONPath = require('jsonpath');
+var JSONPath = require('JSONPath');
  
 
 //Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
@@ -14,9 +14,10 @@ var GET_FACT_MESSAGE = "Here's the river level in CFS: ";
 var HELP_MESSAGE = "You can say what's the current river level, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
+var alexa;
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -32,8 +33,8 @@ var handlers = {
             // Parse the response into a JSON object ready to be formatted.
             var responseData = JSON.parse(response);
 
-            var streamFlow = JSONPath({json: response, path: '$.value.timeSeries[?(@.variable.variableName=="Streamflow, ft&#179;/s")].values[0].value[0].value}'});
-//            var streamFlow = 650;
+            //var streamFlow = JSONPath({json: response, path: '$.value.timeSeries[?(@.variable.variableName=="Streamflow, ft&#179;/s")].values[0].value[0].value}'});
+            var streamFlow = 650;
             // Check if we have correct data, If not create an error speech out to try again.
             if (responseData == null) {
                 output = "There was a problem with getting data please try again";

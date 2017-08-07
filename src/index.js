a539456a9d6f2a718840c71e1062a633d561f2f3
cimadaren <darenarnold1@Darens-MBP.fios-router.home>
2017-08-07 16:03:37 -0400

    Updated help

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 'use strict';
+var http = require('http');
 var https = require('https');
 var Alexa = require('alexa-sdk');
 
@@ -11,7 +12,7 @@ var APP_ID = "amzn1.ask.skill.bdcd7c4c-439f-4673-939c-235d30b6f193";
 
 var SKILL_NAME = "Cheat River Float Trip";
 var GET_FACT_MESSAGE = "Here's the river level in CFS: ";
-var HELP_MESSAGE = "You can say what's the current river level, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can say what's the current river level, what's the current weather, what is the water temperature, should I take the trip or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye!";
 var alexa;
@@ -27,9 +28,45 @@ var handlers = {
     'LaunchRequest': function () {
         console.log("In LaunchRequest");
         this.emit('GetDischargeIntent');
+    }, 
+    'GetWeatherIntent': function () {
+        //
+        // USGS Water Information System REST API details
+        //
+        var options = {
+            host: 'api.openweathermap.org',
+            port: 80,
+            path: '/data/2.5/weather?zip=26287,us&appid=ca54ac9876a10a706d1409e275337f27&units=imperial',
+            method: 'GET'
+        };
+        httpGet(options, http, function (response) {
+            // Parse the response into a JSON object ready to be formatted.
+            var responseData = JSON.parse(response);
+
+            var weatherDesc = JSONPath({json: responseData, path: '$.weather[0].description'});
+            var weatherTemp = JSONPath({json: responseData, path: '$.main.temp'});
+            
+            // Check if we have correct data, If not create an error speech out to try again.
+            if (responseData == null) {
+                output = "There was a problem with getting data please try again";
+            }
+            
+            var weatherText = 'Current weather for Parsons.  The temperature is ' + weatherTemp +  ' fahrenheit.  The conditions are ' + weatherDesc;
+
+            alexa.emit(':tell', weatherText);
+        });
     },
     'GetDischargeIntent': function () {
-        httpGet(function (response) {
+        //
+        // USGS Water Information System REST API details
+        //
+        var options = {
+            host: 'waterservices.usgs.gov',
+            port: 443,
+            path: '/nwis/iv/?site=03069500&format=json',
+            method: 'GET'
+        };
+        httpGet(options, https, function (response) {
             // Parse the response into a JSON object ready to be formatted.
             var responseData = JSON.parse(response);
 
@@ -45,8 +82,60 @@ var handlers = {
             alexa.emit(':tell', dischargeText);
         });
     },
+    'GetRecommendationIntent': function () {
+        //
+        // USGS Water Information System REST API details
+        //
+        var options = {
+            host: 'waterservices.usgs.gov',
+            port: 443,
+            path: '/nwis/iv/?site=03069500&format=json',
+            method: 'GET'
+        };
+        httpGet(options, https, function (response) {
+            // Parse the response into a JSON object ready to be formatted.
+            var responseData = JSON.parse(response);
+
+	    var recText = "";
+
+            var streamFlow = JSONPath({json: responseData, path: '$.value.timeSeries[?(@.variable.variableName=="Streamflow, ft&#179;/s")].values[0].value[0].value'});
+
+            // Check if we have correct data, If not create an error speech out to try again.
+            if (responseData == null) {
+                output = "There was a problem with getting data please try again";
+            }
+            
+	    if (streamFlow < 100)
+		recText = "Water is too low for paddling";
+	    else if (streamFlow > 100 && streamFlow < 250)
+		recText = "Water is crystal clear and beautiful! Great for hot summer days when getting out of your boat is fun. It will be necessary to get out of the boat and drag through shallow areas – we recommend short trips. This water level is good for tubing.";
+	    else if (streamFlow >= 250 && streamFlow < 500)
+		recText = "At this level the river is still low and may require some dragging. The fish are biting in the deeper holes.";
+	    else if (streamFlow >= 500 && streamFlow < 1000)
+		recText = "This is A great water level. The potential still exists to get hung up on some rocks – learning to read the river really helps!";
+	    else if (streamFlow >= 1000 && streamFlow < 5000)
+		recText = "The river is moving more rapidly, meaning less paddling and more floating. Stay away from river banks to avoid tree limbs (strainers) and other hidden debris just under the surface of the water.";
+	    else if (streamFlow >= 5000 && streamFlow < 8000)
+		recText = "Water is moving fast. This level requires boating experience and knowledge of river safety.";
+	    else if (streamFlow > 8000)
+		recText = "Conditions for EXPERTS ONLY. Water moving at this level is extremely swift and requires expert knowledge and experience to negotiate. Do not overestimate your skill level and avoid putting your life in danger!"
+
+	    var introText = "The stream flow is " + streamFlow + " cubic feet per second. The cheat river water trail website says ";
+
+            alexa.emit(':tell', introText + recText);
+        });
+    },
     'GetTemperatureIntent': function () {
-        httpGet(function (response) {
+        //
+        // USGS Water Information System REST API details
+        //
+        var options = {
+            host: 'waterservices.usgs.gov',
+            port: 443,
+            path: '/nwis/iv/?site=03069500&format=json',
+            method: 'GET'
+        };
+        httpGet(options, https, function (response) {
             // Parse the response into a JSON object ready to be formatted.
             var responseData = JSON.parse(response);
 
@@ -79,19 +168,11 @@ var handlers = {
 };
 
 // Create a web request and handle the response.
-function httpGet(callback) {
+function httpGet(options, protocol, callback) {
     
-    //
-    // USGS Water Information System REST API details
-    //
-    var options = {
-        host: 'waterservices.usgs.gov',
-        port: 443,
-        path: '/nwis/iv/?site=03069500&format=json',
-        method: 'GET'
-    };
 
-    var req = https.request(options, (res) => {
+
+    var req = protocol.request(options, (res) => {
 
             var body = '';
 
